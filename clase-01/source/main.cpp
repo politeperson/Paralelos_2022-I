8aef5f68383f9765061e0555532540ec7623423b
politeperson <saul.rojas@ucsp.edu.pe>
2022-03-17 10:45:31 -0500

    Código 01 actualizado

--- clase-01/source/main.cpp
+++ clase-01/source/main.cpp
@@ -4,35 +4,18 @@
 #include <chrono>
 #include <stdlib.h>
 using namespace std;
+
 #define MAX 10000
-
-int ge_random_int(int a, int b) {
-	std::random_device rd;  //Will be used to obtain a seed for the random number engine
-	std::mt19937 gen(rd()); //Standard mersenne_twister_engine seeded with rd()
-	std::uniform_int_distribution<> distrib(a, b);
-
-	return distrib(gen);
-}
-
 
 int main()
 {
 	vector<vector<double>> A(MAX, vector<double>(MAX, 0));
 	vector<double> x(MAX, 0), y(MAX, 0);
 
-	//double A[MAX][MAX], x[MAX], y[MAX];
-	// initializing A, x and y with 0's
-	/*for (int i = 0; i < MAX; ++i) {
-		x[i] = y[i] = 0;
-		for (int j = 0; j < MAX; ++j) {
-			A[i][j] = 0;
-		}
-	}*/
-
 	auto t_start = std::chrono::high_resolution_clock::now();
 
-	// doing some calculations on the arrays
-	// First way
+	// Primer par de bucles
+	// Accediendo por filas
 	for (int i = 0; i < MAX; ++i) {
 		for (int j = 0; j < MAX; ++j) {
 			y[i] += A[i][j] * x[j];
@@ -40,10 +23,12 @@ int main()
 	}
 	auto t_end = std::chrono::high_resolution_clock::now();
 	double elapsed_time_ms = std::chrono::duration<double, std::milli>(t_end - t_start).count();
+	// la diferencia de tiempos se calcula en milisegundos
 	std::cout << "First way (ms): " << elapsed_time_ms << "\n";
 	
 	t_start = std::chrono::high_resolution_clock::now();
-	// assigning y = 0
+	// Segundo par de bucles
+	// Accediendo por columnas
 	for (int j = 0; j < MAX; ++j) {
 		for (int i = 0; i < MAX; ++i) {
 			y[i] += A[i][j] * x[j];
